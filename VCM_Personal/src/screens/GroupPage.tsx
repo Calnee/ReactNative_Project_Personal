cfaa06c61a958886e41b0ea451c01a3963ce0956
Devapriya L <devapriyakodinjoor24@gmail.com>
2024-02-27 09:17:51 +0530

    image picker commit

--- VCM_Personal/src/screens/GroupPage.tsx
+++ VCM_Personal/src/screens/GroupPage.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CommonButton from '../components/CommonButton';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MediaType, launchImageLibrary, ImageLibraryOptions } from 'react-native-image-picker';
 
 const GroupPage = () => {
-    return(
-        <View style={styles.mainStyle}>
-            <Text>Group Page</Text>
-            <CommonButton title={'Browse'} passedFunction={''} values={{
-                emailValue: '',
-                pswdValue: ''
-            }} functionality={''} statusCode={''} />
-        </View>
-    );
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+
+  const OpenCamera = async () => {
+    const options: ImageLibraryOptions = {
+        mediaType: 'photo',  // Specify the media type (photo or video)
+        maxWidth: 800,       // Maximum width of the selected image
+        maxHeight: 600,      // Maximum height of the selected image
+        quality: 0.8,        // Image quality, a number between 0 and 1
+        includeBase64: false, // Whether to include the image as base64-encoded string
+        selectionLimit: 1,   // Limit the number of selected images
+      };
+    try {
+      const result = await launchImageLibrary(options);
+      console.log(result);
+      if (!result.didCancel && result.assets) {
+        const selectedUri = result.assets[0]?.uri;
+        setSelectedImage(selectedUri); // Set the selected image URI
+      }
+    } catch (error) {
+      console.error('Error selecting image:', error);
+    }
+  }
+  return (
+    <View style={styles.mainStyle}>
+      <Text>Group Page</Text>
+      <TouchableOpacity style={styles.buttonStyle} onPress={OpenCamera}>
+        <Text style={styles.textStyel}>
+          Choose img
+        </Text>
+      </TouchableOpacity>
+      {selectedImage && (
+        <Image
+          source={{ uri: selectedImage }}
+          style={{ width: 200, height: 200, marginTop: 20, alignSelf: 'center' }}
+        />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    mainStyle: {
-      backgroundColor:'white',
-      padding:20,
-      height:800
-    },
-    
-  });
+  mainStyle: {
+    backgroundColor: 'white',
+    padding: 20,
+    height: 800,
+  },
+  buttonStyle: {
+    borderWidth: 0.7,
+    borderRadius: 20,
+    width: 160,
+    height: 70,
+    backgroundColor: 'blue',
+    alignSelf: 'center',
+  },
+  textStyel: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontSize: 22,
+    marginTop: 15,
+  }
+});
+
 export default GroupPage;
